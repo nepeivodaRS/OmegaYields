8997f74ff5fa4d9cd2b9e9c1cdfdabde3e31e53e
nepeivodaRS <roman.nepeivoda@hep.lu.se>
2023-04-25 19:06:44 +0200

    UpdDates

--- analyze_tree_withEff.C
+++ analyze_tree_withEff.C
@@ -8,9 +8,9 @@
 
   .L analyze_tree_withEff.C+
 
-  analyze_tree_withEff("./outputTreesDatLists/mc_tree_all.dat", "./outputEff/mc_Eff_6april_injected.root", "./outputAnal/mc_6april_effCorr.root", 0, 1)
+  analyze_tree_withEff("./outputTreesDatLists/mc_tree_all.dat", "./outputEff/mc_Eff_25april_injected.root", "./outputAnal/mc_25april_effCorr.root", 0, 1)
 
-  analyze_tree_withEff("./outputTreesDatLists/data_tree_all.dat", "./outputEff/mc_Eff_6april_injected.root", "./outputAnal/data_6april_effCorr.root", 0, 0)
+  analyze_tree_withEff("./outputTreesDatLists/data_tree_all.dat", "./outputEff/mc_Eff_25april_injected.root", "./outputAnal/data_25april_effCorr.root", 0, 0)
 */
 
 void InitHists(){
